42b800961e8362c034c1c2478d4140bfbea1bf19
yair_grc <yairgarcia7314@gmail.com>
2022-06-13 18:52:24 -0500

    Added functionality to meeting point selection view
    Changes:
      - Dynamic rendering to places tiles on slider
      - Saving selected place and date to localstorage

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card outlined color="transparent">
-    <v-row v-for="item in items" :key="item.id" class="mt-10">
+    <v-row v-for="product in cartProducts" :key="product._id" class="mt-10">
       <v-col cols="12" sm="2">
         <v-img
-          :src="item.src"
+          :src="src(product)"
           max-height="500"
           max-width="500"
           class="grey lighten-2 imagenAspecto2"
@@ -12,16 +12,22 @@
       </v-col>
       <v-col cols="12" sm="8" class="ml-5">
         <v-row>
-          <v-btn x-small text fab id="izquierda">
+          <v-btn
+            x-small
+            text
+            fab
+            id="izquierda"
+            @click="deleteFromCart(product._id)"
+          >
             <v-icon small>mdi-close-circle</v-icon>
           </v-btn>
         </v-row>
         <v-row class="textoSemi mt-7">
-          {{ item.title }}
+          {{ product.title }}
         </v-row>
         <v-row class="textoBasic mt-6">
-          <v-col cols="12" sm="6"> Talla: {{ item.talla }} </v-col>
-          <v-col cols="12" sm="6"> Precio: ${{ item.precio }} </v-col>
+          <v-col cols="12" sm="6"> Talla: {{ product.size }} </v-col>
+          <v-col cols="12" sm="6"> Precio: ${{ product.price }} </v-col>
         </v-row>
       </v-col>
     </v-row>
@@ -29,28 +35,25 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "List",
 
   data: () => {
-    return {
-      items: [
-        {
-          id: 1,
-          src: require("../assets/img-producto/imagen4.jpg"),
-          title: "Sueter Blanco Encaje",
-          talla: "M",
-          precio: "120",
-        },
-        {
-          id: 2,
-          src: require("../assets/img-producto/imagen1.jpg"),
-          title: "Gorra Zara",
-          talla: "CH",
-          precio: "90",
-        },
-      ],
-    };
+    return {};
+  },
+
+  methods: {
+    ...mapActions(["deleteFromCart"]),
+    src(product) {
+      let src = require("../assets/img-producto/" + product.src);
+      return src;
+    },
+  },
+
+  computed: {
+    ...mapGetters({ cartProducts: "getCartProducts" }),
   },
 };
 </script>
