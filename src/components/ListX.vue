6bf7a8e611e17bdc941293eda046c144c536acd0
yair_grc <yairgarcia7314@gmail.com>
2022-06-13 18:52:17 -0500

    Added dynamic cart elements rendering to order summary view

--- src/components/ListX.vue
+++ src/components/ListX.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card outlined color="transparent">
-    <v-row v-for="item in items" :key="item.id" class="mt-10">
+    <v-row v-for="product in cartProducts" :key="product._id" class="mt-10">
       <v-col cols="12" sm="2">
         <v-img
-          :src="item.src"
+          :src="src(product)"
           max-height="500"
           max-width="500"
           class="grey lighten-2 imagenAspecto2"
@@ -12,11 +12,11 @@
       </v-col>
       <v-col cols="12" sm="8" class="ml-5">
         <v-row class="textoSemi mt-7">
-          {{ item.title }}
+          {{ product.title }}
         </v-row>
         <v-row class="textoBasic mt-6">
-          <v-col cols="12" sm="6"> Talla: {{ item.talla }} </v-col>
-          <v-col cols="12" sm="6"> Precio: ${{ item.precio }} </v-col>
+          <v-col cols="12" sm="6"> Talla: {{ product.size }} </v-col>
+          <v-col cols="12" sm="6"> Precio: ${{ product.price }} </v-col>
         </v-row>
       </v-col>
     </v-row>
@@ -24,28 +24,24 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "ListX",
 
   data: () => {
-    return {
-      items: [
-        {
-          id: 1,
-          src: require("../assets/img-producto/imagen4.jpg"),
-          title: "Sueter Blanco Encaje",
-          talla: "M",
-          precio: "120",
-        },
-        {
-          id: 2,
-          src: require("../assets/img-producto/imagen1.jpg"),
-          title: "Gorra Zara",
-          talla: "CH",
-          precio: "90",
-        },
-      ],
-    };
+    return {};
+  },
+
+  methods: {
+    src(product) {
+      let src = require("../assets/img-producto/" + product.src);
+      return src;
+    },
+  },
+
+  computed: {
+    ...mapGetters({ cartProducts: "getCartProducts" }),
   },
 };
 </script>
