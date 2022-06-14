024241a2bde167a9d4d7900759abe91d4c44b8cf
yair_grc <yairgarcia7314@gmail.com>
2022-06-13 20:38:16 -0500

    Added alert when trying to add a product already in the cart

--- src/components/SnackAlert.vue
+++ src/components/SnackAlert.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="text-center">
-    <v-btn dark color="orange darken-2" @click="snackbar = true">
+    <!-- <v-btn dark color="orange darken-2" @click="visible = true">
       Open Snackbar
-    </v-btn>
+    </v-btn> -->
 
-    <v-snackbar v-model="snackbar" :timeout="timeout">
-      {{ text }}
-
+    <v-snackbar
+      v-model="visible"
+      rounded="pill"
+      color="red darken-4"
+      elevation="24"
+    >
+      <strong>{{ text }}</strong>
       <template v-slot:action="{ attrs }">
-        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+        <v-btn color="black" text v-bind="attrs" @click="toggleValidity">
           Close
         </v-btn>
       </template>
@@ -17,12 +21,20 @@
 </template>
 
 <script>
+import { mapGetters, mapMutations } from "vuex";
+
 export default {
-  name: "Carrucel",
-  data: () => ({
-    snackbar: false,
-    text: "Este artículo ya se encuentra en tu carrito.",
-    timeout: 2000,
-  }),
+  name: "SnackAlert",
+  props: {
+    // visible: false,
+    text: String,
+  },
+  data: () => ({}),
+  computed: {
+    ...mapGetters({ visible: "getValidState" }),
+  },
+  methods: {
+    ...mapMutations(["toggleValidity"]),
+  },
 };
 </script>
