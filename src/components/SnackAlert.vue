a5e1845e6b2509b4a61d9477e25963dcc70582a3
yair_grc <yairgarcia7314@gmail.com>
2022-06-13 20:37:36 -0500

    Removed unuseful code

---
+++ src/components/SnackAlert.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-center">
+    <v-btn dark color="orange darken-2" @click="snackbar = true">
+      Open Snackbar
+    </v-btn>
+
+    <v-snackbar v-model="snackbar" :timeout="timeout">
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carrucel",
+  data: () => ({
+    snackbar: false,
+    text: "Este artículo ya se encuentra en tu carrito.",
+    timeout: 2000,
+  }),
+};
+</script>
